09f72566810179ac5d109604cdddec42a7cfbcf9
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2021-09-07 15:30:54 -0300

    First episode completed

---
+++ projeto-sds3/backend/sds3/src/test/java/br/com/carv/sds3/Sds3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carv.sds3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sds3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
